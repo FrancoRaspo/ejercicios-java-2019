cc4bd9c9c2a2a09c2e0e898a82843fce3536e19d
franco.raspo <fraspo@eiva.com.ar>
2019-06-24 15:32:34 -0300

    Agregado de metodo aleman

--- ejercicio-01-prestamo/src/main/java/com/eiv/prestamo/App.java
+++ ejercicio-01-prestamo/src/main/java/com/eiv/prestamo/App.java
@@ -12,6 +12,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import com.eiv.prestamo.metodos.Metodo;
+import com.eiv.prestamo.metodos.MetodoAlemanImpl;
 import com.eiv.prestamo.metodos.MetodoFrancesImpl;
 
 public class App {
@@ -55,16 +56,22 @@ public class App {
         
         try(Scanner scanner = new Scanner(System.in)) {
             
+        	System.out.println("Ingrese sistema (1)FRANCES o (2)ALEMAN: ");
+            Integer sistema = scanner.nextInt();
+            PrestamoDatos.SistemaAmortizacionEnum sistemacalculo=PrestamoDatos.SistemaAmortizacionEnum.of(sistema);
+            
             System.out.println("Ingrese capital del prestamo: ");
             BigDecimal capital = scanner.nextBigDecimal();
             
             System.out.println("Ingrese cantidad de cuotas del prestamo: ");
             Integer cuotas = scanner.nextInt();
+                         
+
+        	System.out.println("Ingrese tasa nominal anual: ");
+        	BigDecimal tna = scanner.nextBigDecimal();
+
             
-            System.out.println("Ingrese tasa nominal anual: ");
-            BigDecimal tna = scanner.nextBigDecimal();
-            
-            PrestamoDatos prestamoDatos = new PrestamoDatos(capital, cuotas, tna);
+            PrestamoDatos prestamoDatos = new PrestamoDatos(capital, cuotas, tna, sistemacalculo);
             PrestamoDatos.esValido(prestamoDatos);
             
             return Optional.of(prestamoDatos);
@@ -82,45 +89,33 @@ public class App {
     
     public List<Cuota> calcularCuotas(PrestamoDatos prestamoDatos) {
         
-        int nrocuotas = prestamoDatos.getCuotas();
+    	Metodo metodo=null;
+    	
+    	if (prestamoDatos.getSistema()==PrestamoDatos.SistemaAmortizacionEnum.FRANCES) {
+    		metodo = new MetodoFrancesImpl();
+    	}
+    	else if (prestamoDatos.getSistema()==PrestamoDatos.SistemaAmortizacionEnum.ALEMAN) {
+    		metodo = new MetodoAlemanImpl();
+    		
+    	}
+    	
+    	return metodo.calcularCuotas(prestamoDatos);
         
-        BigDecimal razon = prestamoDatos.getTna()
-                .multiply(DIAS)
-                .divide(CIEN.multiply(DIAS_ANIO), 6, RoundingMode.HALF_UP);
-        
-        BigDecimal valorCuota = calculoValorCuota(prestamoDatos);
-        BigDecimal saldoCapital = prestamoDatos.getCapital();
-        
-        LOG.debug("Valor de cuota calculada: " + valorCuota);
-        
-        List<Cuota> cuotas = new ArrayList<>();
-        
-        for(int i = 0; i < nrocuotas; i++) {
-            
-            BigDecimal interes = saldoCapital.multiply(razon)
-                    .setScale(2, RoundingMode.HALF_UP);
-            
-            BigDecimal capitalCuota = valorCuota.subtract(interes);
-            
-            saldoCapital = saldoCapital.subtract(capitalCuota);
-            
-            Cuota cuota = new Cuota(
-                    i + 1, 
-                    capitalCuota, 
-                    interes, 
-                    saldoCapital);
-            
-            cuotas.add(cuota);
-            
-            LOG.debug("Procesada cuota: " + (i + 1));
-        }
-        
-        return cuotas;
+       
     }
     
     public BigDecimal calculoValorCuota(PrestamoDatos prestamoDatos) {
+    	Metodo metodo=null;
+    	
+    	if (prestamoDatos.getSistema()==PrestamoDatos.SistemaAmortizacionEnum.FRANCES) {
+    		metodo = new MetodoFrancesImpl();
+    	}
+    	else if (prestamoDatos.getSistema()==PrestamoDatos.SistemaAmortizacionEnum.ALEMAN) {
+    		metodo = new MetodoAlemanImpl();
+    		
+    	}
+    	
+    	return metodo.calculoValorCuota(prestamoDatos);
         
-        Metodo metodo = new MetodoFrancesImpl();
-        return metodo.calculoValorCuota(prestamoDatos);
     }
 }
--- ejercicio-01-prestamo/src/main/java/com/eiv/prestamo/metodos/MetodoFrancesImpl.java
+++ ejercicio-01-prestamo/src/main/java/com/eiv/prestamo/metodos/MetodoFrancesImpl.java
@@ -2,12 +2,21 @@ package com.eiv.prestamo.metodos;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import com.eiv.prestamo.App;
+import com.eiv.prestamo.Cuota;
 import com.eiv.prestamo.PrestamoDatos;
 
 public class MetodoFrancesImpl implements Metodo {
-    
+	  
+	private static final Logger LOG = LogManager.getLogger(MetodoFrancesImpl.class);
+	
+	
     @Override
     public BigDecimal calculoValorCuota(PrestamoDatos prestamoDatos) {
 
@@ -25,4 +34,42 @@ public class MetodoFrancesImpl implements Metodo {
         return valorCuota;
     }
 
+    public List<Cuota> calcularCuotas(PrestamoDatos prestamoDatos) {
+        
+        int nrocuotas = prestamoDatos.getCuotas();
+        
+        BigDecimal razon = prestamoDatos.getTna()
+                .multiply(App.DIAS)
+                .divide(App.CIEN.multiply(App.DIAS_ANIO), 6, RoundingMode.HALF_UP);
+        
+        BigDecimal valorCuota = calculoValorCuota(prestamoDatos);
+        BigDecimal saldoCapital = prestamoDatos.getCapital();
+        
+        LOG.debug("Valor frances de cuota calculada: " + valorCuota);
+        
+        List<Cuota> cuotas = new ArrayList<>();
+        
+        for(int i = 0; i < nrocuotas; i++) {
+            
+            BigDecimal interes = saldoCapital.multiply(razon)
+                    .setScale(2, RoundingMode.HALF_UP);
+            
+            BigDecimal capitalCuota = valorCuota.subtract(interes);
+            
+            saldoCapital = saldoCapital.subtract(capitalCuota);
+            
+            Cuota cuota = new Cuota(
+                    i + 1, 
+                    capitalCuota, 
+                    interes, 
+                    saldoCapital);
+            
+            cuotas.add(cuota);
+            
+            LOG.debug("Procesada cuota: " + (i + 1));
+        }
+        
+        return cuotas;
+    }
+    
 }
